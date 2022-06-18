9f57ee946a9aa3c7f076127836615b4ee6632f14
igor655 <belyyigor@gmail.com>
2022-06-18 08:25:17 +0300

    Initial commit
    * Add
    
    	new file:   CppProject_3/CppProject_3.cpp
    	modified:   CppProject_3/CppProject_3.vcxproj
    	modified:   CppProject_3/CppProject_3.vcxproj.filters

---
+++ CppProject_3/CppProject_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// указатель на константу
+	// константный указатель
+	// константный указатель на константу
+	// Прата стр 333
+
+	int a = 45;
+
+	const int* ptr_a = &a;
+
+
+	return 0;
+}
