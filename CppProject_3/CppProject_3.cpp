174d1a57d98455429d1e25336bf7350d01bca857
igor655 <belyyigor@gmail.com>
2022-06-18 08:34:04 +0300

    Edit CppProject_3.cpp
    * Add #include<Windows.h>
    *Add setlocale(LC_ALL, "ru");
    
    	SetConsoleCP(1251);
    	SetConsoleOutputCP(1251);

--- CppProject_3/CppProject_3.cpp
+++ CppProject_3/CppProject_3.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include<Windows.h>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	setlocale(LC_ALL, "ru");
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	// указатель на константу
 	// константный указатель
 	// константный указатель на константу
@@ -11,6 +16,15 @@ int main(int argc, char* argv[])
 	int a = 45;
 
 	const int* ptr_a = &a;
+
+	cout << "указатель на константу = " << *ptr_a << endl;
+
+	char ch[10];
+
+	cin >> ch;
+
+	cout << ch << endl;
+	
 
 
 	return 0;
